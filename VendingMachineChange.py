aa1e8aa4a47687df07c0423bb6de0a961c5b29de
Joshua Alexander <91466077+joshualxndrs@users.noreply.github.com>
2021-12-21 16:21:53 +0700

    added lines and thank you

--- VendingMachineChange.py
+++ VendingMachineChange.py
@@ -4,6 +4,7 @@ print("Coke Medium = $1.75")
 print("Fanta Medium = $1.80")
 print("Sprite Medium = $1.70")
 
+print("----------------------------------------------")
 
 cost = float(input("Input the drink cost: "))
 give = float(input("Type Amount given: "))
@@ -13,6 +14,8 @@ while cost <= 0:
 
 while give <= cost or give <= 0:
     give = float(input("Amount paid should be equal or more than cost: "))
+
+print("----------------------------------------------")
 
 coins=[]
 
@@ -33,6 +36,12 @@ pen=coins.count(1)
 
 dollars = int(change)
 if half !=0 or quarter != 0 or dime!=0 or nickel!=0 or pen!=0:
-    print ('The change of', round(give,2), 'is:',change, '\n-dollars:',dollars,'\n-halfs(50):',half,'\n-quarters(25):',quarter,'\n-dime(10):',dime,'\n-nickels(5):',nickel,'\n-pennies(1):',pen)
+    print ('The change of', round(give,2), 'is:','%.2f'%change, '\n-dollars:',dollars,'\n-halfs(50):',half,'\n-quarters(25):',quarter,'\n-dime(10):',dime,'\n-nickels(5):',nickel,'\n-pennies(1):',pen)
 else:
      print ('The change from', round(give,2), 'is:',change,'and no coins')
+
+
+print("----------------------------------------------")
+
+
+print("THANK YOU FOR ORDERING!")
